c0017b77530da510cf97812d211d0d63eb0f9aea
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2024-04-08 10:57:18 -0400

    got started on the home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header"
+// import Footer from './components/Footer'
+
+import Home from "./pages/Home"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Home />} />
+            {/* <Route path="projects" element={<Projects />} />
+            <Route path="resume" element={<Resume />} />
+            <Route path="about" element={<About />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
